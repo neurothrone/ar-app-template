9b68c09d15bb7a30e0e1d5e13f66fb2b8154b440
Zaid Neurothrone <54468049+neurothrone@users.noreply.github.com>
2022-10-16 04:57:06 +0200

    Initial Commit

---
+++ AppTemplateSetUpAR/AppTemplateSetUpARApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppTemplateSetUpARApp.swift
+//  AppTemplateSetUpAR
+//
+//  Created by Zaid Neurothrone on 2022-10-16.
+//
+
+import SwiftUI
+
+@main
+struct AppTemplateSetUpARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
